68671c045766668d9d9c9d4104ae6d8e9f487092
unknown <timkachern@mail.ru>
2025-07-28 12:58:55 +0300

    feat: implement core API with Docker support

---
+++ server/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/TimofeyChernyshev/MusicStreamingService/db"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.StaticFile("/", "./frontend/player/index.html")
+	r.Static("/player-static", "./frontend/player")
+	r.Static("/storage/cover", "./storage/cover")
+
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_HOST"),
+	)
+
+	repo_song, err := db.NewSongRepository(connStr)
+	if err != nil {
+		log.Println(err)
+	}
+	defer repo_song.Close()
+
+	repo_album, err := db.NewAlbumRepository(connStr)
+	if err != nil {
+		log.Println(err)
+	}
+	defer repo_album.Close()
+
+	r.GET("api/albums", func(c *gin.Context) {
+		repo_album.GetAlbums(c)
+	})
+
+	r.GET("api/albums/:id/tracks", func(c *gin.Context) {
+		repo_song.GetTracks(c)
+	})
+
+	r.GET("api/stream", func(c *gin.Context) {
+		track := c.Query("track")
+		if track == "" {
+			log.Println("error: track parameter is required")
+			c.JSON(http.StatusBadRequest, gin.H{"error": "track parameter is required"})
+			return
+		}
+
+		streamingURL := fmt.Sprintf(os.Getenv("STREAM_URL")+"/stream?track=%s", url.QueryEscape(track))
+
+		resp, err := http.Get(streamingURL)
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		defer resp.Body.Close()
+
+		for k, v := range resp.Header {
+			c.Header(k, v[0])
+		}
+		c.Status(resp.StatusCode)
+		c.Stream(func(w io.Writer) bool {
+			io.Copy(w, resp.Body)
+			return false
+		})
+	})
+
+	log.Fatal(r.Run(":8080"))
+}
